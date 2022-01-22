e74df96d8051a028541f52f0314b1cae5da342cf
Andrew Polk <polkandrew01@gmail.com>
2022-01-22 11:34:49 -0800

    Improve starting word calculation

--- WordleTest.py
+++ WordleTest.py
@@ -1,5 +1,6 @@
-import json
+import json, re
 from multiprocessing import Pool
+from sys import exc_info
 from Wordle import formatDictionary, getStartGuesses, main, loadDictionary, wordDict
 from tqdm import tqdm
 
@@ -9,20 +10,42 @@ formatDictionary()
 # plumb
 
 def tryWord(guess):
+    size = len(guess)
     failedCount = 0
-    for word in wordDict[5]:
-    # for word in tqdm(wordDict[5]):
+    for word in wordDict[size]:
+    # for word in tqdm(wordDict[size]):
         if main(guess, word) == False:
             failedCount += 1
-    errorRate = failedCount/len(wordDict[5])*100
+    errorRate = failedCount/len(wordDict[size])*100
     print(guess + ' - Failed: ' + str(round(errorRate, 2)) + '%')
     return [errorRate, guess]
+
+def CalcUnknownWords(size=5):
+    values = []
+    with Pool(12) as p:
+        values += p.map(tryWord, getStartGuesses(size)[:120])
+
+    print('> Starting Pool 2')
+    with Pool(12) as p:
+        values += p.map(tryWord, getStartGuesses(size)[-120:])
+
+    values = sorted(values)
+
+    excludeLetters = ''
+    results = []
+    for i in range(len(values)):
+        value = values[i][1]
+        ex = excludeLetters
+        if ex == '':
+            ex = '.'
+        regex = '[^' + ex + ']{'+str(size)+'}'
+        if bool(re.match(regex, value)) == True:
+            results.append(value)
+            excludeLetters += "".join(set(value))
+    print('Best starting words')
+    print(results)
 
 if __name__ == "__main__":
     # tryWord('about')
     # tryWord('plumb')
-    with Pool(12) as p:
-        out = p.map(tryWord, getStartGuesses(5)[-120:])
-        
-        with open('results2.json', 'w') as f:
-            f.write(json.dumps(out, indent=2))
+    CalcUnknownWords(6)
